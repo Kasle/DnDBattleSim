6e444951d427f29e0846da0401491d5bf738c000
Aleks Mercik <aleks.mercik@outlook.com>
2017-01-30 13:17:39 -0500

    redid some token stuff

--- tokens.py
+++ tokens.py
@@ -3,14 +3,25 @@ import random
 class creature:
 
     def __init__(self):
-        self.ac = random.randint(12, 17)
-        self.att = 0
-        self.hp = roll(3, 8) + 5
-        self.weap = 6
-        self.dambns = 3
+
+        self.level = 1
+        self.HPDice = 8
+        
+        self.AC = 10
+        
+        self.STR = 0
+        self.CHA = 0
+        self.DEX = 0
+        self.INT = 0
+        self.CON = 0
+        self.WIS = 0
+
+        self.weapon = [6]
+
+        self.HP = self.HPDice + self.CON*self.level + roll(self.level-1, self.HPDice)
     
     def attack(self):
-        return [roll(1, 20)+self.att, roll(1, self.weap)+self.dambns]
+        return [roll(1, 20)+self.STR, roll(1, self.weap)+self.dambns]
 
 def roll(count, die):
     return sum([random.randint(1, die) for i in range(count)])
